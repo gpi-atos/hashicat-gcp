aec4046970b3791ef897e931a64f16290de05b3c
GPI <georges.pisant@atos.net>
2024-01-12 19:01:34 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   project_id   = var.project
   network_name = "gaurav-network"
-  subnet = [
+  subnets = [
     {
       subnet_name   = "gaurav-subnet"
       subnet_ip     = "10.100.10.0/24"
