0e7801ea8909bc02f18a881f54acca5109a330d4
GPI <georges.pisant@atos.net>
2024-01-12 18:46:03 +0000

    new remote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-<<<<<<< HEAD
     organization = "evdn-oc-gcp-gpi-sentinel"
-=======
-    organization = "evdn-oc-gcp-gpi-tf"
->>>>>>> 5b9e17a9511ee83df95f1326010bb33d182251a9
     workspaces {
       name = "hashicat-gcp"
     }
